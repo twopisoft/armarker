2dbc77aa5cba7690cd43a9a72f4c055a5c794f3c
twopisoft <twopisoft@gmail.com>
2014-02-26 11:28:38 +0800

    Initial Commit

---
+++ ARMarker/com_twopiAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  com_twopiAppDelegate.h
+//  ARMarker
+//
+//  Created by TwoPi on 26/2/14.
+//  Copyright (c) 2014 TwoPi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface com_twopiAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ARMarker/com_twopiViewController.h
@@ -0,0 +1,13 @@
+//
+//  com_twopiViewController.h
+//  ARMarker
+//
+//  Created by TwoPi on 26/2/14.
+//  Copyright (c) 2014 TwoPi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface com_twopiViewController : UIViewController
+
+@end
